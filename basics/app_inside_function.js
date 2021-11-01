ef5aea4a108d36a2b0efe7d32930ee98b91aca8f
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-11-01 16:18:15 +0530

    Node JS Basic

---
+++ basics/app_inside_function.js
@@ -0,0 +1,7 @@
+(function (exports, require, module, __filename, __dirname) {
+	console.log('Exports - ', exports);
+	console.log('Require - ', require);
+	console.log('Module - ', module);
+	console.log('File Name - ', __filename);
+	console.log('Directory Name - ', __dirname);
+})
---
+++ basics/basic_node_js.js
@@ -0,0 +1,17 @@
+/*
+//	Global Objects -
+console
+setTimeout();
+clearTimeout();
+setInterval();
+clearInterval();*/
+
+console.log(module);
+//global.console.log(module);
+
+console.log('Directory Name - ', __dirname);
+
+//	Template String
+//	ES6 / ES2015 / ECMAScript 6
+var fileName = __filename;
+console.log(`File Name - ${fileName}`);
